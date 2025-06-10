bcb03cc9a7b6f8be3490db310ff69d8ad6f487a7
mjjjing <2856891664@qq.com>
2025-06-10 17:01:05 +0800

    2.5修改启动文案

--- src/main/java/com/mjjjing/wiki/WikiApplication.java
+++ src/main/java/com/mjjjing/wiki/WikiApplication.java
@@ -1,13 +1,20 @@
 package com.mjjjing.wiki;
 
+import org.apache.juli.logging.LogFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class WikiApplication {
-
+    public static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
     public static void main(String[] args) {
-        SpringApplication.run(WikiApplication.class, args);
+        SpringApplication app = new SpringApplication(WikiApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("start success!!");
+        LOG.info("address:\thttp://127.0.0.1:{}",env.getProperty("server.port"));
     }
 
 }
