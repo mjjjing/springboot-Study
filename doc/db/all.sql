8cca72fffac7721215137a236757fa953e6273f6
mjjjing <2856891664@qq.com>
2025-06-11 00:20:05 +0800

    添加热部署依赖，保存sql代码

---
+++ doc/db/all.sql
@@ -0,0 +1,6 @@
+drop table if exists test;
+create table test (
+    id bigint not null comment 'id' primary key ,
+    name varchar(50) comment  '名称',
+    password varchar(50) comment  '密码'
+)engine = innodb default charset=utf8mb4 comment = '测试'
