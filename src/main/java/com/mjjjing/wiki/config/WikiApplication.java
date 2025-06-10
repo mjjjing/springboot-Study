888c30e45c9c6d7ae7a5910dda4d6036c5009d8f
mjjjing <2856891664@qq.com>
2025-06-10 17:39:29 +0800

    2.6 开发hello world接口 访问地址：http://127.0.0.1:8080/hello

--- src/main/java/com/mjjjing/wiki/config/WikiApplication.java
+++ src/main/java/com/mjjjing/wiki/config/WikiApplication.java
@@ -1,12 +1,13 @@
-package com.mjjjing.wiki;
+package com.mjjjing.wiki.config;
 
-import org.apache.juli.logging.LogFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
+@ComponentScan("com.mjjjing")
 @SpringBootApplication
 public class WikiApplication {
     public static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
---
+++ src/main/java/com/mjjjing/wiki/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.mjjjing.wiki.controller;
+
+import org.springframework.web.bind.annotation.*;
+//请求注解
+@RestController
+public class TestController {
+
+    //GET,POST,PUT,DELETE
+    /*@GetMapping
+    @PostMapping
+    @PutMapping
+    @DeleteMapping
+    @RequestMapping支持所有形式的请求
+    @RequestMapping(value = "/user/1",method = RequestMethod.GET)
+    @RequestMapping(value = "/user/1",method = RequestMethod.DELETE)*/;
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello world!!";
+    }
+}
