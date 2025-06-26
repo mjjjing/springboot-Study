d0d8411fcb1e195fe8e5c00224bdf7204cf3875a
mjjjing <2856891664@qq.com>
2025-06-26 21:34:01 +0800

    3.7 封装请求参数和返回参数

--- src/main/java/com/mjjjing/wiki/controller/EbookController.java
+++ src/main/java/com/mjjjing/wiki/controller/EbookController.java
@@ -1,7 +1,8 @@
 package com.mjjjing.wiki.controller;
 
-import com.mjjjing.wiki.domain.Ebook;
+import com.mjjjing.wiki.req.EbookReq;
 import com.mjjjing.wiki.resp.CommonResp;
+import com.mjjjing.wiki.resp.EbookResp;
 import com.mjjjing.wiki.service.EbookService;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,9 +21,9 @@ public class EbookController {
     public EbookService ebookService;
 
     @GetMapping("/list")
-    public CommonResp list(){
-        CommonResp<List<Ebook>> resp = new CommonResp<>();
-        List<Ebook> list = ebookService.list();
+    public CommonResp list(EbookReq req){
+        CommonResp<List<EbookResp>> resp = new CommonResp<>();
+        List<EbookResp> list = ebookService.list(req);
         resp.setContent(list);
         return resp;
     }
--- src/main/java/com/mjjjing/wiki/service/EbookService.java
+++ src/main/java/com/mjjjing/wiki/service/EbookService.java
@@ -1,10 +1,15 @@
 package com.mjjjing.wiki.service;
 
 import com.mjjjing.wiki.domain.Ebook;
+import com.mjjjing.wiki.domain.EbookExample;
 import com.mjjjing.wiki.mapper.EbookMapper;
+import com.mjjjing.wiki.req.EbookReq;
+import com.mjjjing.wiki.resp.EbookResp;
 import jakarta.annotation.Resource;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -12,8 +17,23 @@ public class EbookService {
     @Resource
     private EbookMapper ebookMapper;
 
-    public List<Ebook> list(){
-        return ebookMapper.selectByExample(null);
+    public List<EbookResp> list(EbookReq req){
+        EbookExample ebookExample = new EbookExample();
+        EbookExample.Criteria criteria = ebookExample.createCriteria();
+        //模糊匹配
+        criteria.andNameLike("%"+req.getName()+"%");
+        //把所有匹配的实例放入ebookList里面
+        List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
+
+        List<EbookResp> respList = new ArrayList<>();
+        //遍历ebookList,目的为把Ebook转换成EbookResp
+        for (Ebook ebook : ebookList) {
+            EbookResp ebookResp = new EbookResp();
+            BeanUtils.copyProperties(ebook,ebookResp);
+            respList.add(ebookResp);
+        }
+
+        return  respList;
     }
 
 }
