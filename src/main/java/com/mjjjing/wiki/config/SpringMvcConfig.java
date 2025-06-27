8be298197c7a36e9c3b624d3cae26fd816e731df
mjjjing <2856891664@qq.com>
2025-06-28 02:28:05 +0800

    5.9 增加AOP，打印接口耗时、请求参数和返回参数 去掉过滤器、拦截器

--- src/main/java/com/mjjjing/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/mjjjing/wiki/config/SpringMvcConfig.java
@@ -1,20 +1,20 @@
-package com.mjjjing.wiki.config;
-
-import com.mjjjing.wiki.interceptor.LogInterceptor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import jakarta.annotation.Resource;
-
-@Configuration
-public class SpringMvcConfig implements WebMvcConfigurer {
-
-    @Resource
-    LogInterceptor logInterceptor;
-
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(logInterceptor)
-                .addPathPatterns("/**").excludePathPatterns("/login");
-    }
-}
+//package com.mjjjing.wiki.config;
+//
+//import com.mjjjing.wiki.interceptor.LogInterceptor;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import jakarta.annotation.Resource;
+//
+//@Configuration
+//public class SpringMvcConfig implements WebMvcConfigurer {
+//
+//    @Resource
+//    LogInterceptor logInterceptor;
+//
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(logInterceptor)
+//                .addPathPatterns("/**").excludePathPatterns("/login");
+//    }
+//}
