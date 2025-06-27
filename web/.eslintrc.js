7f105508c894e37c6dca5be4b31d74157f948f31
mjjjing <2856891664@qq.com>
2025-06-28 01:15:14 +0800

    5.5 修改bug，重新配置一遍多环境

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -6,7 +6,7 @@ module.exports = {
   'extends': [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/typescript/recommended'
+    '@vue/typescript/recommended',
   ],
   parserOptions: {
     ecmaVersion: 2020
@@ -14,6 +14,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'vue/no-unused-components' : 'off'
+    'vue/no-unused-components' : 'off',
+    "@typescript-eslint/no-explicit-any": ["off"]
   }
 }
