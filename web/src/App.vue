701e204c425bf23e40737d03d6a4aab421983c45
mjjjing <2856891664@qq.com>
2025-06-27 18:02:23 +0800

    4.7 制作the-footer组件

--- web/src/App.vue
+++ web/src/App.vue
@@ -2,18 +2,18 @@
   <a-layout>
     <the-header></the-header>
     <router-view/>
-    <a-layout-footer style="text-align: center">
-      mjjjing电子书
-    </a-layout-footer>
+    <the-footer></the-footer>
   </a-layout>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import TheHeader from "@/components/the-header.vue";
+import TheFooter from "@/components/the-footer.vue";
 export default defineComponent({
   name: 'app',
   components:{
+    TheFooter,
     TheHeader,
   }
 });
